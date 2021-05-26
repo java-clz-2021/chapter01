17c510d31dfa5b3c4cd8ea44fac2d8d72fa265ab
javaclz2021 <javaclz2021@gmail.com>
2021-05-26 16:45:56 +0900

    월표시 문제: switch~case문 break문 활용

---
+++ src/com/javaex/ex03/Ex12.java
@@ -0,0 +1,36 @@
+package com.javaex.ex03;
+
+import java.util.Scanner;
+
+public class Ex12 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int month = sc.nextInt();
+
+		
+		//4 6 9 11 --> 30일
+		//2        --> 28일
+		//나머지   --> 31일
+		
+		switch (month) {
+			case 4:
+			case 6:	
+			case 9:
+			case 11:	
+				System.out.println("30일");
+				break;
+			case 2:
+				System.out.println("28일");
+				break;
+			default:
+				System.out.println("31일");
+				break;
+		}
+		
+		System.out.println("안녕!!");
+		sc.close();
+	}
+
+}
